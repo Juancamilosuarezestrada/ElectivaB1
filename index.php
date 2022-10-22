432fb4cf51d18e189454519c8d73861b4ea30785
ESTUDIANTE <sergio.sil.27@hotmail.com>
2022-10-21 19:39:55 -0500

    front

--- index.php
+++ index.php
@@ -1,12 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="login.php">dasdas</a>
-</body>
+<html lang="es">
+    
+<?php include ("layouts/header.php") ?>
+    <link rel="stylesheet" href="../css/style.css">
+       
+    
+<!-- /HEADER -->
+
+        <!-- SECTION -->
+        <div class="section">
+            <!-- container -->
+            <!-- Background image -->
+  <div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url('https://concepto.de/wp-content/uploads/2015/03/paisaje-800x409.jpg');
+      height: 400px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Heading</h1>
+          <h4 class="mb-3">Subheading</h4>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+          >Call to action</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Background image -->
+
+        </div>
+        <!-- /SECTION -->
+<br><br>
+        
+<!--/Sub Footer-->
+<?php include ("layouts/footer.php") ?>
+       
+    </body>
 </html>
